3608ed122be9110779e0e9e2dacf5ed83de7a5b1
bwongSR <bryan.wong@satoriresearch.io>
2024-09-03 11:58:24 +0800

    temporary main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
-import express from "express";
+// import express from "express";
 
-const app = express();
-const port = 4200;
+// const app = express();
+// const port = 4200;
 
-app.get("/ping", (req, res) => {
-	res.send("pong");
-});
+// app.get("/ping", (req, res) => {
+// 	res.send("pong");
+// });
 
-app.listen(port, () => {
-	console.log(`Listening on port ${port}...`);
-});
+// app.listen(port, () => {
+// 	console.log(`Listening on port ${port}...`);
+// });
 
-export default app;
+// export default app;
+
+// src/main.ts
+import { type Server } from "bun";
+
+export default {
+	async fetch(request: Request, server: Server) {
+		return new Response("Hello from Bun on Vercel", {
+			status: 200,
+			headers: { "Content-Type": "text/plain" },
+		});
+	},
+};
